984d0da5631caea75fdcfb37d3f91d2e988084f0
KatieKosh <kkoshinsnki@hotmail.com>
2017-03-11 20:46:33 -0600

    initial commit

---
+++ train.js
@@ -0,0 +1,19 @@
+
+var config = {
+    apiKey: "AIzaSyBJFik0pbYbIFadGaZXn36gfsBwU6RFM2o",
+    authDomain: "train-bc2f2.firebaseapp.com",
+    databaseURL: "https://train-bc2f2.firebaseio.com",
+    storageBucket: "train-bc2f2.appspot.com",
+    messagingSenderId: "444993654646"
+  };
+  firebase.initializeApp(config);
+
+
+
+// $(document).ready(function()
+//
+// var train sporty spice = moment() + 25
+// console.log train sporty spice
+//
+//
+// )};
